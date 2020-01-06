feebbab3b04d3217093b6592308a50e6af97e2dd
Preved <underproof2014@gmail.com>
2020-01-06 15:10:48 +0300

    fix problem with API-url

--- constants/constants.js
+++ constants/constants.js
@@ -1,8 +1,5 @@
 export const STATIC_URL = '/'
 
-export const API_URL = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return ''
-  }
-  return 'http://localhost:3000'
-}
+// eslint-disable-next-line prettier/prettier
+export const API_URL =
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
